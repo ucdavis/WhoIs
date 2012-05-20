7ca2cb4f6c1f07af6fd388e0a76909be4e4a77d8
Scott Kirkland <srkirkland@gmail.com>
2012-05-19 22:27:43 -0700

    can view list of all users

---
+++ WhoIs/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Mvc;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Web.Controller;
+using WhoIs.Models;
+
+namespace WhoIs.Controllers
+{
+    public class UserController : SuperController
+    {
+        private readonly IRepositoryWithTypedId<User, Guid> _userRepository;
+
+        public UserController(IRepositoryWithTypedId<User,Guid> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        //
+        // GET: /User/
+        public ActionResult Index()
+        {
+            var users = _userRepository.GetAll();
+            
+            return View(users);
+        }
+
+    }
+}
--- WhoIs/Models/User.cs
+++ WhoIs/Models/User.cs
@@ -1,11 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using FluentNHibernate.Mapping;
+using UCDArch.Core.DomainModel;
 
 namespace WhoIs.Models
 {
-    public class User
+    public class User : DomainObjectWithTypedId<Guid>
     {
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string Email { get; set; }
+        public virtual string LoginId { get; set; }
+    }
+
+    public class UserMap : ClassMap<User>
+    {
+        public UserMap()
+        {
+            ReadOnly();
+            Table("MothraWhoIs");
+            Id(x => x.Id).GeneratedBy.Assigned();
+
+            Map(x => x.FirstName);
+            Map(x => x.LastName);
+            Map(x => x.Email);
+            Map(x => x.LoginId);
+        }
     }
 }
